2bd868ecdb96a86e0553a0c546851f89d8d55e46
nisaChampagne <nchampag23@gmail.com>
2019-08-13 23:55:11 -0400

    1 binaries uploaded

--- components/nav.js
+++ components/nav.js
@@ -1,22 +1,44 @@
-    
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Contact from '../components/navcomponents/contact.js';
+import AboutMe from '../components/navcomponents/aboutme.js';
+import Projects from '../components/navcomponents/projects.js';
+import Resume from '../components/navcomponents/resume.js';
 
 
-function Nav (){
- return(
-     <div>
-         <h1 className='name-h1'>Nisa Champagne</h1>
-         <nav className='navs'>
-            <li><a href='index.html' className='nav-a'>Home</a></li>
-            <li><a href='contact.js' className='nav-a'>Contact</a></li>
-            <li><a href='aboutme.js' className='nav-a'>About</a></li>
-            <li><a href='projects.js'
-            className='nav-a'>Projects</a></li>
-            <li><a href='resume.js'
-            className='nav-a'>Resume</a></li>
-         </nav>
-     </div>
- );
+
+function Nav() {
+  return (
+    <Router>
+        <div>
+        <nav className='navs'>
+          <ul>
+            <li className='nav-links'>
+              <Link to="/">Home</Link>
+            </li>
+             <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/projects/">Projects</Link>
+            </li>
+             <li>
+              <Link to="/resume/">Resume</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" />
+        <Route path="/contact/" component={Contact} />
+        <Route path="/about/" component={AboutMe} />
+        <Route path="/projects/" component={Projects} />
+        <Route path="/resume/" component={Resume} />
+      </div>
+    </Router>
+  );
 }
 
 export default Nav;
